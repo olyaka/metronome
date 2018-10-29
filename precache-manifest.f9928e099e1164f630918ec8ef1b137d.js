10f43433d626d2544110c306daf50c4b6c8b2bf0
Olga <fej-d@yandex.ru>
2018-10-29 21:44:48 +0300

    Updates

---
+++ precache-manifest.f9928e099e1164f630918ec8ef1b137d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cb7961e7527a2414076a333634e5feb",
+    "url": "/metronome/static/media/click2.9cb7961e.wav"
+  },
+  {
+    "revision": "36940b90f50b8d90fe36569136786b7a",
+    "url": "/metronome/static/media/click1.36940b90.wav"
+  },
+  {
+    "revision": "c60a8487983b61cb8a18",
+    "url": "/metronome/static/js/runtime~main.c60a8487.js"
+  },
+  {
+    "revision": "54050b5bcde9200cded1",
+    "url": "/metronome/static/js/main.54050b5b.chunk.js"
+  },
+  {
+    "revision": "539f7f5d9619716caefe",
+    "url": "/metronome/static/js/1.539f7f5d.chunk.js"
+  },
+  {
+    "revision": "54050b5bcde9200cded1",
+    "url": "/metronome/static/css/main.ce089d19.chunk.css"
+  },
+  {
+    "revision": "8202a15298a413d3318a2b169ca0a41f",
+    "url": "/metronome/index.html"
+  }
+];
